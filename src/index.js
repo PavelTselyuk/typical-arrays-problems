f30b2c93aebb3e87287649b88358e95f750273cd
Pavel Tselyuk <pavlik_astap@mail.ru>
2021-03-14 05:35:30 +0300

    fix: done different way again

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 
-exports.min = function min (array) {
+exports.min = function min(array) {
   let minElem = 0;
-  for( let i = 0; i < array.length; i++ ){
+  if (!array || array.length < 1) {return 0};
+  for (let i = 0; i < array.length; i++) {
     minElem = array[i] < minElem ? array[i] : minElem;
+  }
   return minElem;
 }
 
-exports.max = function max (array) {
+exports.max = function max(array) {
   let maxElem = 0;
-  for( let i = 0; i < array.length; i++ ){
+  if (!array || array.length < 1) {return 0};
+  for (let i = 0; i < array.length; i++) {
     maxElem = array[i] > maxElem ? array[i] : maxElem;
-  return maxElem;
+  }
+    return maxElem;
 }
 
-exports.avg = function avg (array) {
-  return array && array.length > 0 ? array.reduce((sum, current) => sum + current, 0)/array.length : 0;
+exports.avg = function avg(array) {
+  return array && array.length > 0 ? array.reduce((sum, current) => sum + current, 0) / array.length : 0;
 }
